48d2fa13c83ddb63b673d2897104078355edc0a1
ankush <ankush.dev2@gmail.com>
2019-07-23 17:51:19 +0530

    Three sum with hash

--- src/com/ankushgrover/problems/P11ThreeSumBool.java
+++ src/com/ankushgrover/problems/P11ThreeSumBool.java
@@ -7,31 +7,61 @@ public class P11ThreeSumBool {
 
     public static void main(String[] args) {
 
-        int[] arr = { 1, 4, 45, 6, 10, 8 };
-        System.out.println(threeSum(arr, 56));
+        int[] arr = {8,8,8,8,8,8,8,8};
+        int target = 25;
 
+        long prev = System.nanoTime();
+
+        System.out.println(threeSumHashing(arr, target));
+        System.out.println("Time Taken: " + (System.nanoTime() - prev));
+
+        prev = System.nanoTime();
+
+        System.out.println(threeSum(arr, target));
+        System.out.println("Time Taken: " + (System.nanoTime() - prev));
+    }
+
+    private static boolean threeSumHashing(int[] arr, int target) {
+
+        for (int i = 0; i < arr.length - 2; i++) {
+
+            int subTarget = target - arr[i];
+            HashSet<Integer> set = new HashSet<>();
+
+            for (int j = i + 1; j < arr.length; j++) {
+
+                if (set.contains(subTarget - arr[j])) {
+                    System.out.println(String.format("[%d, %d, %d]", arr[i], arr[j], subTarget - arr[j]));
+                    return true;
+                }
+
+                set.add(arr[j]);
+            }
+        }
+
+        return false;
     }
 
     private static boolean threeSum(int[] arr, int target) {
 
         Arrays.sort(arr);
 
-        for(int i = 0 ; i < arr.length-2; i++){
+        for (int i = 0; i < arr.length - 2; i++) {
 
-            int start = i+1;
-            int end = arr.length-1;
+            int start = i + 1;
+            int end = arr.length - 1;
 
             int subTarget = target - arr[i];
 
-            while(start < end){
+            while (start < end) {
                 int sum = arr[start] + arr[end];
-                if(sum == subTarget){
+                if (sum == subTarget) {
                     System.out.println(String.format("[%d, %d, %d]", arr[i], arr[start], arr[end]));
                     return true;
                 }
 
 
-                if(sum < subTarget)
+                if (sum < subTarget)
                     start++;
                 else end--;
             }
@@ -39,4 +69,6 @@ public class P11ThreeSumBool {
         }
         return false;
     }
+
+
 }
