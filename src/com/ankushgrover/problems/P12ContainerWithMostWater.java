4b5a8fcecaa5502594c4b9d9a49af3db842bf9df
ankush <ankush.dev2@gmail.com>
2019-07-24 23:07:47 +0530

    P12

---
+++ src/com/ankushgrover/problems/P12ContainerWithMostWater.java
@@ -0,0 +1,29 @@
+package com.ankushgrover.problems;
+
+/**
+ * https://leetcode.com/problems/cos
+ * ntainer-with-most-water/
+ */
+public class P12ContainerWithMostWater {
+
+    public static void main(String[] args) {
+        //{2, 3, 10, 5, 7, 8, 9};
+        int[] arr = {2, 3, 10, 5, 7, 8, 9};
+        System.out.println(maxArea(arr));
+    }
+
+
+    private static int maxArea(int[] height) {
+        int start = 0;
+        int end = height.length - 1;
+        int max = 0;
+        while (start < end) {
+            max = Math.max(max, Math.min(height[start], height[end]) * (end - start));
+
+            if (height[end] < height[start])
+                end--;
+            else start++;
+        }
+        return max;
+    }
+}
