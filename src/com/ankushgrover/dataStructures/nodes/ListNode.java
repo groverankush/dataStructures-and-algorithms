bcaa228c1863ed991a2b58fbf136e9fffe97d2be
ankush <ankush.dev2@gmail.com>
2019-07-28 22:23:39 +0530

    ListNode

--- src/com/ankushgrover/dataStructures/nodes/ListNode.java
+++ src/com/ankushgrover/dataStructures/nodes/ListNode.java
@@ -1,4 +1,29 @@
 package com.ankushgrover.dataStructures.nodes;
 
 public class ListNode {
+    public int val;
+    public ListNode next;
+
+    public ListNode(int x) {
+        val = x;
+    }
+
+    public static ListNode create(int... nums) {
+        ListNode head = new ListNode(nums[0]);
+        ListNode node = head;
+        for (int i = 1; i < nums.length; i++) {
+            node.next = new ListNode(nums[i]);
+            node = node.next;
+        }
+
+        return head;
+    }
+
+    public static void print(ListNode node) {
+        while (node != null) {
+            System.out.print(node.val + "->");
+            node = node.next;
+        }
+        System.out.println("null");
+    }
 }
