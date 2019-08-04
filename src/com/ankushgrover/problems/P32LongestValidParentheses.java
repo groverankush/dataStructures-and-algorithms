bc95def3a47b137e232b8792842592bc070dc5d8
ankush <ankush.dev2@gmail.com>
2019-08-04 14:16:23 +0530

    P32

---
+++ src/com/ankushgrover/problems/P32LongestValidParentheses.java
@@ -0,0 +1,38 @@
+package com.ankushgrover.problems;
+
+import java.util.Stack;
+
+public class P32LongestValidParentheses {
+
+    public static void main(String[] args) {
+        System.out.println(longestValidParentheses(")()())"));
+    }
+
+    private static int longestValidParentheses(String s) {
+        Stack<Integer> stack = new Stack<>();
+        int[] arr = new int[s.length()];
+
+        for (int i = 0; i < s.length(); i++) {
+            if (s.charAt(i) == '(') {
+                stack.push(i);
+            } else {
+                if (!stack.isEmpty()) {
+                    arr[i] = 1;
+                    arr[stack.pop()] = 1;
+                }
+            }
+        }
+        int temp = 0;
+        int max = 0;
+        for (int i : arr) {
+            if (i == 1) {
+                temp++;
+            } else {
+                max = Math.max(temp, max);
+                temp = 0;
+            }
+        }
+
+        return Math.max(temp, max);
+    }
+}
