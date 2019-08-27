0998f196a4a53e5411103c5e8d8427cbb4ad6217
Ankush grover <ankush.dev2@gmail.com>
2019-08-27 07:29:00 +0530

    P59

---
+++ src/com/ankushgrover/problems/P59IntersectionOfTwoLinkedList.java
@@ -0,0 +1,57 @@
+package com.ankushgrover.problems;
+
+import com.ankushgrover.dataStructures.nodes.ListNode;
+
+/**
+ * Created by Ankush Grover(ankush.dev2@gmail.com) on 26/08/19
+ *
+ * https://leetcode.com/problems/intersection-of-two-linked-lists/
+ */
+public class P59IntersectionOfTwoLinkedList {
+
+    public static void main(String[] args) {
+    }
+
+    public ListNode getIntersectionNode(ListNode headA, ListNode headB) {
+
+        int l1 = 0, l2 = 0;
+
+        ListNode a = headA, b = headB;
+
+        while(true){
+            if(a!=null){
+                l1++;
+                a = a.next;
+            }
+
+            if(b!=null){
+                l2++;
+                b = b.next;
+            }
+            if(a == null && b == null)
+                break;
+        }
+
+        if(l1>l2){
+            a = headA;
+            b = headB;
+        }
+        else{
+            a = headB;
+            b = headA;
+        }
+
+        for(int i = 0; i < Math.abs(l1-l2); i++){
+            a = a.next;
+        }
+
+        while(a!=null && b!=null){
+            if(a == b)
+                return a;
+            a = a.next;
+            b = b.next;
+        }
+
+        return null;
+    }
+}
