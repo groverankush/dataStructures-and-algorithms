ad6c90e47fe114e84cfb7b29703d7e49918a3ba2
Ankush grover <ankush.dev2@gmail.com>
2019-09-19 08:00:05 +0530

    P119 - The Reader Class

---
+++ src/com/ankushgrover/problems/P119CFDiv2AXeniaAndDivisors.java
@@ -0,0 +1,177 @@
+package com.ankushgrover.problems;
+
+import java.io.DataInputStream;
+import java.io.FileInputStream;
+import java.io.IOException;
+
+/**
+ * Created by Ankush Grover(ankush.dev2@gmail.com) on 19/09/19
+ * <p>
+ * http://codeforces.com/problemset/problem/342/A
+ */
+public class P119CFDiv2AXeniaAndDivisors {
+
+    public static void main(String[] args) throws IOException {
+
+        Reader in = new Reader();
+
+        int n = in.nextInt();
+
+        int[] map = new int[7];
+
+        int num;
+        for (int i = 0; i < n; i++) {
+            num = in.nextInt();
+            if (num == 5 || num == 7) {
+                System.out.print(-1);
+                return;
+            }
+
+            map[num]++;
+        }
+
+        if (map[1] < map[3] || map[6] < map[3]) {
+            System.out.println(-1);
+            return;
+        }
+        StringBuilder b1 = new StringBuilder();
+
+        while (map[3] > 0) {
+            b1.append(1).append(" ").append(3).append(" ").append(6).append("\n");
+            map[3]--;
+            map[1]--;
+            map[6]--;
+        }
+
+        if (map[1] != map[2] || map[1] != (map[4] + map[6])) {
+            System.out.println(-1);
+            return;
+        }
+
+        StringBuilder b2 = new StringBuilder();
+
+        while (map[1] != 0) {
+            if (map[4] != 0) {
+                b2.append(1).append(" ").append(2).append(" ").append(4).append("\n");
+                map[4]--;
+            } else {
+                b2.append(1).append(" ").append(2).append(" ").append(6).append("\n");
+                map[6]--;
+            }
+
+            map[1]--;
+            map[2]--;
+        }
+
+        System.out.print(b2);
+        System.out.print(b1);
+
+    }
+
+    static class Reader {
+        final private int BUFFER_SIZE = 1 << 16;
+        private DataInputStream din;
+        private byte[] buffer;
+        private int bufferPointer, bytesRead;
+
+        public Reader() {
+            din = new DataInputStream(System.in);
+            buffer = new byte[BUFFER_SIZE];
+            bufferPointer = bytesRead = 0;
+        }
+
+        public Reader(String file_name) throws IOException {
+            din = new DataInputStream(new FileInputStream(file_name));
+            buffer = new byte[BUFFER_SIZE];
+            bufferPointer = bytesRead = 0;
+        }
+
+        public String readLine() throws IOException {
+            byte[] buf = new byte[64]; // line length
+            int cnt = 0, c;
+            while ((c = read()) != -1) {
+                if (c == '\n')
+                    break;
+                buf[cnt++] = (byte) c;
+            }
+            return new String(buf, 0, cnt);
+        }
+
+        public int nextInt() throws IOException {
+            int ret = 0;
+            byte c = read();
+            while (c <= ' ')
+                c = read();
+            boolean neg = (c == '-');
+            if (neg)
+                c = read();
+            do {
+                ret = ret * 10 + c - '0';
+            } while ((c = read()) >= '0' && c <= '9');
+
+            if (neg)
+                return -ret;
+            return ret;
+        }
+
+        public long nextLong() throws IOException {
+            long ret = 0;
+            byte c = read();
+            while (c <= ' ')
+                c = read();
+            boolean neg = (c == '-');
+            if (neg)
+                c = read();
+            do {
+                ret = ret * 10 + c - '0';
+            }
+            while ((c = read()) >= '0' && c <= '9');
+            if (neg)
+                return -ret;
+            return ret;
+        }
+
+        public double nextDouble() throws IOException {
+            double ret = 0, div = 1;
+            byte c = read();
+            while (c <= ' ')
+                c = read();
+            boolean neg = (c == '-');
+            if (neg)
+                c = read();
+
+            do {
+                ret = ret * 10 + c - '0';
+            }
+            while ((c = read()) >= '0' && c <= '9');
+
+            if (c == '.') {
+                while ((c = read()) >= '0' && c <= '9') {
+                    ret += (c - '0') / (div *= 10);
+                }
+            }
+
+            if (neg)
+                return -ret;
+            return ret;
+        }
+
+        private void fillBuffer() throws IOException {
+            bytesRead = din.read(buffer, bufferPointer = 0, BUFFER_SIZE);
+            if (bytesRead == -1)
+                buffer[0] = -1;
+        }
+
+        private byte read() throws IOException {
+            if (bufferPointer == bytesRead)
+                fillBuffer();
+            return buffer[bufferPointer++];
+        }
+
+        public void close() throws IOException {
+            if (din == null)
+                return;
+            din.close();
+        }
+    }
+}
