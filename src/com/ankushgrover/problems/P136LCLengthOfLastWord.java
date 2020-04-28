2a1761796db81405f09fc288af7f83051439b2d9
Ankush grover <ankush.dev2@gmail.com>
2020-04-28 13:57:39 +0530

    P136

--- src/com/ankushgrover/problems/P136LCLengthOfLastWord.java
+++ src/com/ankushgrover/problems/P136LCLengthOfLastWord.java
@@ -9,16 +9,31 @@ public class P136LCLengthOfLastWord {
 
     public static void main(String[] args) {
         P136LCLengthOfLastWord obj = new P136LCLengthOfLastWord();
-        System.out.println(obj.lengthOfLastWord("Hello"));
+        System.out.println(obj.lengthOfLastWord02("world"));
     }
 
 
-    int lengthOfLastWord(String s) {
+    private int lengthOfLastWord(String s) {
 
         String arr[] = s.split(" ");
         if (arr.length > 1)
             return arr[arr.length - 1].length();
         return 0;
 
+    }
+
+    private int lengthOfLastWord02(String s) {
+
+        int count = 0;
+        int i = s.length()-1;
+        while(i >= 0){
+            if  (s.charAt(i) == ' ')
+                i--;
+            else break;
+        }
+        for (; i >= 0 && s.charAt(i) != ' '; i--) {
+            count++;
+        }
+        return count;
     }
 }
