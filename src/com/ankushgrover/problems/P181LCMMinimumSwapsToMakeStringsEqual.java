ed546966a18118d0bf08b9f4687eb1f97635fac8
Ankush grover <ankush.dev2@gmail.com>
2020-07-03 21:07:14 +0530

    P181

---
+++ src/com/ankushgrover/problems/P181LCMMinimumSwapsToMakeStringsEqual.java
@@ -0,0 +1,41 @@
+package com.ankushgrover.problems;
+
+/**
+ * Created by Ankush Grover(ankush.dev2@gmail.com) on 03/07/20
+ */
+public class P181LCMMinimumSwapsToMakeStringsEqual {
+
+    public static void main(String[] args) {
+        P181LCMMinimumSwapsToMakeStringsEqual obj = new P181LCMMinimumSwapsToMakeStringsEqual();
+        System.out.println(obj.minimumSwap("xx", "yy"));
+    }
+
+
+    public int minimumSwap(String s1, String s2) {
+        if (s1.length() != s2.length())
+            return -1;
+
+        if (s1.length() == 1 && s1.charAt(0) != s2.charAt(0))
+            return -1;
+
+        int x1[] = new int[2];
+        int x2[] = new int[2];
+
+
+        for (int i = 0; i < s1.length(); i++) {
+            if (s1.charAt(i) != s2.charAt(i)) {
+                x1[s1.charAt(i) - 'x']++;
+                x2[s2.charAt(i) - 'x']++;
+            }
+        }
+        if (x1[0] != x2[1] || x1[1] != x2[0])
+            return -1;
+
+        int count = (x1[0] / 2) + (x1[1] / 2);
+        x1[0] %= 2;
+        x1[1] %= 2;
+        x2[1] %= 2;
+        x2[0] %= 2;
+        return x1[0] != x1[1] ? -1 : count + (x1[0] * 2);
+    }
+}
