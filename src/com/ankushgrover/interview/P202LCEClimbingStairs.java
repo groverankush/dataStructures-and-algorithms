b7383cf0678e9c64b5b7d5666d8bc68bf9fa7ba9
Ankush grover <ankush.dev2@gmail.com>
2020-08-01 02:56:50 +0530

    P202

---
+++ src/com/ankushgrover/interview/P202LCEClimbingStairs.java
@@ -0,0 +1,27 @@
+package com.ankushgrover.interview;
+
+/**
+ * Created by Ankush Grover(ankush.dev2@gmail.com) on 01/08/20
+ * <p>
+ * https://leetcode.com/problems/climbing-stairs/
+ * <p>
+ * #easy #fibinacci
+ */
+public class P202LCEClimbingStairs {
+
+    public static void main(String[] args) {
+    }
+
+    public int climbStairs(int n) {
+        int first = 0;
+        int sec = 1;
+
+        for (int i = 0; i < n; i++) {
+            int sum = first + sec;
+            first = sec;
+            sec = sum;
+        }
+
+        return sec;
+    }
+}
