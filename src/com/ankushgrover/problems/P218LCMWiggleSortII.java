0d9d52519425767e0e38598f21549b477403e2a4
Ankush grover <ankush.dev2@gmail.com>
2021-04-14 14:20:51 +0530

    P218

---
+++ src/com/ankushgrover/problems/P218LCMWiggleSortII.java
@@ -0,0 +1,33 @@
+package com.ankushgrover.problems;
+
+import java.util.Arrays;
+
+/**
+ * Created by Ankush Grover(ankush.dev2@gmail.com) on 14/04/21
+ * <p>
+ * https://leetcode.com/problems/wiggle-sort-ii/
+ */
+public class P218LCMWiggleSortII {
+
+    public static void main(String[] args) {
+
+    }
+
+    public void wiggleSort(int[] nums) {
+
+        int arr[] = Arrays.copyOf(nums, nums.length);
+
+        Arrays.sort(arr);
+
+        int index = nums.length - 1;
+
+        for (int i = 1; i < nums.length; i += 2) {
+            nums[i] = arr[index--];
+        }
+
+        for (int i = 0; i < nums.length; i += 2) {
+            nums[i] = arr[index--];
+        }
+
+    }
+}
