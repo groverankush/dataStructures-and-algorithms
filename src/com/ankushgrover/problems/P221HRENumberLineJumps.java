2806f938c3d759107b567650871b041d096f34c4
Ankush grover <ankush.dev2@gmail.com>
2021-04-14 17:46:15 +0530

    P221

---
+++ src/com/ankushgrover/problems/P221HRENumberLineJumps.java
@@ -0,0 +1,25 @@
+package com.ankushgrover.problems;
+
+/**
+ * Created by Ankush Grover(ankush.dev2@gmail.com) on 14/04/21
+ */
+public class P221HRENumberLineJumps {
+
+    public static void main(String[] args) {
+    }
+
+    static String kangaroo(int x1, int v1, int x2, int v2) {
+
+        double res = (x2 - x1) / ((v1 - v2) * 1.0);
+
+        if (isInt(res) && res > 0.f) {
+            return "YES";
+        }
+
+        return "NO";
+    }
+
+    static boolean isInt(double d) {
+        return d == (int) d;
+    }
+}
