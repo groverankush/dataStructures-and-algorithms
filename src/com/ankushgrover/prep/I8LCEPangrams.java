43f235358ebc7b7ad765b9fd337fea18b6f7e9da
Ankush grover <ankush.dev2@gmail.com>
2021-04-20 20:02:48 +0530

    I8

---
+++ src/com/ankushgrover/prep/I8LCEPangrams.java
@@ -0,0 +1,31 @@
+package com.ankushgrover.prep;
+
+/**
+ * Created by Ankush Grover(ankush.dev2@gmail.com) on 20/04/21
+ */
+public class I8LCEPangrams {
+
+    public static void main(String[] args) {
+
+    }
+
+    String pangrams(String s) {
+
+        int arr[] = new int[26];
+
+        for (int i = 0; i < s.length(); i++) {
+            char ch = s.charAt(i);
+            if (Character.isAlphabetic(ch)) {
+                arr[Character.toLowerCase(ch) - 'a']++;
+            }
+        }
+
+        for (int i = 0; i < arr.length; i++) {
+            if (arr[i] == 0)
+                return "not pangram";
+        }
+
+        return "pangram";
+
+    }
+}
